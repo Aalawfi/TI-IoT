1c5f8e41496a1dceb0e1c340605d7b10d39b26a1
Abdullah Alawfi <asalawfi@outlook.com>
2022-04-13 16:02:47 -0400

    Display registered devices

--- front-end/src/views/protected-views/Devices.js
+++ front-end/src/views/protected-views/Devices.js
@@ -7,29 +7,27 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 
 function Devices() {
-
+    // Collects data about who is currently logged in
+    const base_URL = window.location.href
+    const base_URL_clear = base_URL.replace("/devices", "")
+    let last_index_of_slash = base_URL_clear.lastIndexOf("/")
+    const current_user_const = (base_URL_clear.substring(last_index_of_slash+1, base_URL_clear.length))
 
   const [devices, setDevices] = useState([])
 
 
-  useEffect( () => {
-      
-    axios.get("http://localhost:8000/Alawfi/api/get-devices/")
-    .then( (response) => { response.data.map( (device,index) => 
-      {     
-      
-      setDevices([
-                  ...devices,
-                  {
-                    id: device.id,
-                    device_name: device.device_name
-                  }
-                ])
-      } 
-    ) })
-   
-    }, []
-  )
+    // fetch the current device 
+    useEffect( () => {  
+      // at server environemnt, use https://www.ti-fi-uofsc.com/${current_user_const}/api/get-devices/
+      axios.get(`http://localhost:8000/${current_user_const}/api/get-devices/`)
+      .then( (response) => { 
+
+        response.data.map( (element) => { setDevices(prevState => [...prevState, element.device_name]) } )
+        console.log(devices)
+
+      })  
+      }, [current_user_const]
+    )
 
   return (
     <div className="dashboard-container">
@@ -41,13 +39,16 @@ function Devices() {
               <h1> Devices content </h1>
           </div>
           <div className="dashboard-content-body-devices"> 
-              <h3> Registered boards </h3>
-              {/* TODO display devices list*/}
-              {devices.map( (device, key) => { console.log(device.device_name)} )}
+          
+              <h3> Registered devices </h3>
+            
+              {devices.map( (element) => { return <li> {element} </li>} )} 
+              
+             
           </div>
         </div>
 
-        
+         
     </div>
   );
 }
