dcfb13d2dfd572a1afba443b95a2a4e621da50d3
Abdullah Alawfi <asalawfi@outlook.com>
2022-10-12 14:57:14 -0400

    Added SMS functionality into views.py

--- serverside/api/views.py
+++ serverside/api/views.py
@@ -1,3 +1,4 @@
+# Django utils
 from django.http import JsonResponse
 from django.shortcuts import render
 
@@ -9,6 +10,10 @@ from rest_framework.parsers import JSONParser
 
 from userspool.models import Userpool
 from .serializers import *
+
+# SMS app 
+from .sms_app import send_sms
+import threading
 
 # based on a user, return JSON the name of the user
 @api_view(['GET'])
@@ -46,17 +51,32 @@ def get_all_data(request, user_name, device_name):
 # TODO post some
 @api_view(['POST'])
 def post_data(request, user_name, device_name):
-
+    user_name = user_name.capitalize()
     device_name = device_name.lower()
+    
     # find the pk of the device
     requested_device = RegisteredDevices.objects.get(device_name = device_name).pk
-    print(request.data)
+
+    requested_user = Userpool.objects.get(username=user_name)
+    phone_num = requested_user.phone_num
+    # print(request.data)
 
     Temp = request.data['data']['Tempurature']
     Humd = request.data['data']['Humidity']
     Movement = request.data['data']['Movement']
     Gas = request.data['data']['Gas']
     Generic = request.data['data']['Generic']
+
+    if Temp > 80.0 and not phone_num == '-1':
+    # to enable, change this to false 
+        thread_active = False
+    #    thread_active = False
+    
+        for thread in threading.enumerate():
+            if thread.name == 'sms_thread':
+                thread_active = True
+        if thread_active == False:
+            threading.Thread(name='sms_thread', target=send_sms, args=('+14237417499',)).start()
 
     timestamp = request.data['timestamp']
     destination_device = requested_device
