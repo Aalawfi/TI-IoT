9add1d356edfbacf136c9ffe208163b856ea99d4
Abdullah Alawfi <asalawfi@outlook.com>
2022-10-12 14:55:29 -0400

    Changed Auth token

--- serverside/api/sms_app.py
+++ serverside/api/sms_app.py
@@ -5,9 +5,9 @@ from twilio.rest import Client
 
 def send_sms(phone_num):
 
-    account_sid = 'AC53918ba201c254d14e89d8d869e0b978' #os.environ['TWILIO_ACCOUNT_SID']
-    auth_token = '4944a1c8177c98494d3300bf11c2af68' #os.environ['TWILIO_AUTH']
-    msg_service = 'MG2131c41993dbbdccabfbba33f7342e51' #os.environ['TWILIO_MSG'] 
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH']
+    msg_service = os.environ['TWILIO_MSG'] 
     client = Client(account_sid, auth_token)
     try:
         message = client.messages.create(  
